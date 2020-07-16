740bd1576c934bbd36cd9d7b5af8d16a2c5c632c
Amruth Kuntamalla <amruthk99@gmail.com>
2020-07-17 02:38:39 +0530

    dep

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes/api");
 const path = require("path");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 //Serving the build files
 const DIST_DIR = path.join(__dirname, "./client/dist");

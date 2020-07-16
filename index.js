c11c18705e3494cf660a50ce938e46d7f32c2dca
Amruth Kuntamalla <amruthk99@gmail.com>
2020-07-17 02:44:36 +0530

    dep

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes/api");
 const path = require("path");
 require("dotenv").config();
-const port = process.env.PORT;
-
+const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");

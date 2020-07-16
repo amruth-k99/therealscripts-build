408244a8817a6a523b8b389af7aecc8b8f067f3c
Amruth Kuntamalla <amruthk99@gmail.com>
2020-07-17 02:29:52 +0530

    dep

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ app.use((req, res, next) => {
 });
 
 //connect to the database
-mongoose
-  .connect(process.env.DB, { useNewUrlParser: true })
-  .then(() => console.log(`Database connected successfully`))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(process.env.DB, { useNewUrlParser: true })
+//   .then(() => console.log(`Database connected successfully`))
+//   .catch((err) => console.log(err));
 
 //since mongoose promise is depreciated, we overide it with node's promise
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 
